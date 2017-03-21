9c04b0a0124803c47e0a2365e92a10425a1c36fa
Ryo Kawaguchi <ryo@istellar.jp>
2017-03-21 15:41:14 +0900

    Rename to the service name to Connect and fix profile image URLs.

--- server/middleware/view-helper.js
+++ server/middleware/view-helper.js
@@ -3,7 +3,8 @@ var config = require('../config/main');
 var moment = require('moment');
 
 module.exports = function(req, res, next) {
-  // TODO(ryok): Move moment here.
+  res.locals.serviceName = config.serviceName;
+  res.locals.shortServiceName = config.shortServiceName;
   res.locals.path = req.path;
   res.locals.googleAnalytics = secrets.googleAnalytics;
   res.locals.base_url = config.baseUrl;
