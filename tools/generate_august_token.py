1a74fa07dc0c4ba2d83c12cc30a64594cbbcad0c
Ryo Kawaguchi <ryo@kawagu.ch>
2020-12-31 12:34:52 +0900

    Make generate_august_token.sh non-interactive so it can be scheduled as a cron

--- tools/generate_august_token.py
+++ tools/generate_august_token.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python
 
+import os
 import sys
+import time
+import re
 
 from august.api import Api, HEADER_VALUE_API_KEY
 from august.authenticator import Authenticator, AuthenticationState, ValidationResult
 
+def read_last_verification_code():
+    with open('/var/mail/ubuntu', 'r') as f:
+        lines = f.read().splitlines()
+        for i in range(1, 6):
+            m = re.search(r'[0-9]{6}', lines[-1 * i])
+            if m:
+                return m.group(0)
+
 api = Api(timeout=20)
 
-print('ENTER PASSWORD:')
-password = input()
-authenticator = Authenticator(api, "email", "ryo@straylight.jp", password)
+password = os.environ['AUGUST_PASSWORD']
+authenticator = Authenticator(api, "email", "august@mail.straylight.jp", password)
 auth = authenticator.authenticate()
 
 if auth.state == AuthenticationState.BAD_PASSWORD:
@@ -19,8 +29,9 @@ if auth.state == AuthenticationState.BAD_PASSWORD:
 if auth.state == AuthenticationState.REQUIRES_VALIDATION:
     authenticator.send_verification_code()
 
-    print('ENTER VERIFICATION CODE (sent to email):')
-    code = input()
+    time.sleep(30)
+
+    code = read_last_verification_code()
     validation_result = authenticator.validate_verification_code(code)
 
     if validation_result != ValidationResult.VALIDATED:
@@ -29,8 +40,4 @@ if auth.state == AuthenticationState.REQUIRES_VALIDATION:
 
     auth = authenticator.authenticate()
 
-print('API KEY:')
-print(HEADER_VALUE_API_KEY)
-print('ACCESS TOKEN:')
 print(auth.access_token)
-
